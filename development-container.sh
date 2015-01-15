f7eee48f0590d719fae6df337a0c41df97e47ab3
Ryan Roberts <ryansroberts@gmail.com>
2015-01-15 13:13:58 +0000

    Source build mono / fsharp. It's the only way to be sure

--- development-container.sh
+++ development-container.sh
@@ -3,5 +3,5 @@
 docker build -t "nice/ldci" docker/build/
 docker kill ldci 
 docker rm ldci
-docker run -d --name linkeddatabuild -v $(pwd):/src -t nice/ldci
+docker run -d --name ldci -v $(pwd):/src/ -t nice/ldci
 
