e61c31899ea6e4ff2b39e170579c18253a2c253d
Ryan Roberts <ryansroberts@gmail.com>
2015-01-16 10:03:18 +0000

    Build in drone

--- development-container.sh
+++ development-container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t "nice/ldci" docker/build/
+docker build -t "nice/ldci" docker/develop/
 docker kill ldci 
 docker rm ldci
 docker run -d --name ldci -v $(pwd):/src/ -t nice/ldci
