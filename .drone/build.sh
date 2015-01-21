6c708920beb7b3b3c0f47661ff622ef0f7f1bc71
Ryan Roberts <ryansroberts@gmail.com>
2015-01-21 17:14:19 +0000

    Auth dockerhub

--- .drone/build.sh
+++ .drone/build.sh
@@ -8,7 +8,7 @@ cd $DRONE_BUILD_DIR
 
 wrapdocker &  
 sleep 5
-
+docker login --email=$DOCKERHUB_EMAIL --username=$DOCKERHUB_USER --password=$DOCKERHUB_PASS
 docker build -t nice/mimir .  
 docker push nice/mimir ../
 ./build.sh 
