9797f3e1c94d16dc36ce4126f29fc12b621e9d51
Ryan Roberts <ryansroberts@gmail.com>
2015-01-22 15:03:04 +0000

    Do private stuff via env vars - drone will pick up DRONE_ ..hopefully

--- .drone/build.sh
+++ .drone/build.sh
@@ -10,7 +10,7 @@ cd $DRONE_BUILD_DIR
 wrapdocker &  
 sleep 5
 
-echo docker login -e $1 -u $2 -p $3
+docker login -e $DRONE_DOCKERHUB_EMAIL -u $DRONE_DOCKERHUB_USER -p $DRONE_DOCKERHUB_PASS
 docker build -t nice/mimir .  
 docker push nice/mimir ../
 ./build.sh 
