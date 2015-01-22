76a0310776a6ffa727797b1e01e3c638335dc0bd
Ryan Roberts <ryansroberts@gmail.com>
2015-01-22 15:21:17 +0000

    Well apparently, this was built in :|

--- .drone/build.sh
+++ .drone/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo $(env)
 set -e  
 cd $DRONE_BUILD_DIR
 
@@ -7,10 +6,3 @@ cd $DRONE_BUILD_DIR
 ./build.sh 
 
 
-wrapdocker &  
-sleep 5
-
-docker login -e $DRONE_DOCKERHUB_EMAIL -u $DRONE_DOCKERHUB_USER -p $DRONE_DOCKERHUB_PASS
-docker build -t nice/mimir .  
-docker push nice/mimir ../
-./build.sh 
