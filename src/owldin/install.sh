eaaaae8ea07fdac88c4ec0060707238a022328d9
Charlotte Gore <conspiracygore@gmail.com>
2015-03-09 14:55:20 +0000

    Fixed up repo

--- src/owldin/install.sh
+++ src/owldin/install.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 cd /src/owldin
+# for debugging node... 
 # npm install -g node-inspector
 # node-inspector --web-port=8335 &
-# npm install
-# grunt 
+
+# standard auto development...
+npm install -g grunt-cli
+npm install
+grunt 
+
+# manual restart/build etc. with debugging..
 # node --debug server.js 
-node server.js
+
+# manual restart/build etc. without debugging.. 
+# node server.js
