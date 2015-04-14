d5192c82bd47ea7ac68cde2eff816c405bc93e98
Ryan Roberts <ryansroberts@gmail.com>
2015-04-14 13:58:29 +0100

    Watch scripts

--- tools/install.sh
+++ tools/install.sh
@@ -4,5 +4,6 @@ mono .paket/paket.exe restore
 
 ln -s /tools/freya /usr/bin/freya
 ln -s /tools/git2prov /usr/bin/git2prov
-ln -s /tools/makeworld /usr/bin/makeworld
+ln -s /tools/makewatch /usr/bin/makewatch
+ln -s /tools/makeless /usr/bin/makeless
 
