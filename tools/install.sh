0b53b29e4c6ffad47433a766c8f4a15ac66c3aaf
Ryan Roberts <ryansroberts@gmail.com>
2015-04-23 12:21:33 +0100

    Install these

--- tools/install.sh
+++ tools/install.sh
@@ -6,4 +6,5 @@ ln -s /tools/freya /usr/bin/freya
 ln -s /tools/git2prov /usr/bin/git2prov
 ln -s /tools/makewatch /usr/bin/makewatch
 ln -s /tools/makeless /usr/bin/makeless
+ln -s /tools/makeall /usr/bin/makeall
 
